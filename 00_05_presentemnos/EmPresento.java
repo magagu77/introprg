b9fca5c666cb495ee2720321d7177aab058b0266
Marc Garcia <marc.garcia0502@gmail.com>
2021-09-30 18:54:29 +0200

    Ejercicio 00_05EmPresento acabado

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,7 @@
+/*Programa prin nº2*/
+public class EmPresento {
+    public static void main(String[] args){
+        System.out.println("Hola, me llamo Marc y aspiro a ser un programador regulero. ");
+    }
+}
+
