548a36945ba596130998c6cf2518df78f9024d2d
Marc Garcia <marc.garcia0502@gmail.com>
2021-09-30 19:50:52 +0200

    Ejercicio 01_02_Duplica acabado

---
+++ 01_02_duplica/Duplica.java
@@ -0,0 +1,23 @@
+/*Programa para multiplicar un numero x 2 
+EL programa no controla valores no adecuados de entrada (LETRAS )*/
+
+class Duplica {
+    public static void main(String[] args) {
+    
+    //Declarar variables a utilizar
+    int nombreADuplicar;
+    int nombreDuplicat;
+    
+    //Multplicar número
+    
+    nombreADuplicar = Integer.parseInt(args[0]);
+    
+    //Calcula el doble
+    nombreDuplicat = nombreADuplicar * 2 ;
+    System.out.println ("El doble de ");
+    System.out.println (nombreADuplicar);
+    System.out.println ("és");
+    System.out.println (nombreDuplicat);
+    }
+}
+
