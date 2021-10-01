0d7d995dc88a3333cc01037da509cb4b9956ef75
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-01 15:01:06 +0200

    Cambio de w a W

--- 00_04_holamon/HolaMon.java
+++ 00_04_holamon/HolaMon.java
@@ -1,6 +1,6 @@
 /* Programa que saluda al món */
 public class HolaMon {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
