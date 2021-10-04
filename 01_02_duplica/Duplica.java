d201d397cd5aba5a33dea013022b00b4e4035428
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-04 16:35:48 +0200

    Nombre de carpeta cambiado

---
+++ 01_02_duplica/Duplica.java
@@ -0,0 +1,20 @@
+/*Programa para multiplicar un numero x 2 
+EL programa no controla valores no adecuados de entrada (LETRAS )*/
+
+class Duplica {
+    public static void main(String[] args) {
+    
+    //Declarar variables a utilizar
+    int nombreADuplicar;
+    int nombreDuplicat;
+    
+    //Multplicar número
+    
+    nombreADuplicar = Integer.parseInt(args[0]);
+    
+    //Calcula el doble
+    nombreDuplicat = nombreADuplicar * 2 ;
+    System.out.println ("El doble de " + nombreADuplicar +" és " + nombreDuplicat);
+    }
+}
+
