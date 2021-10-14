0989227633756c51d5fd17c21cf274ec150925cc
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-14 19:58:59 +0200

    ejercicio 01_15 acabado

--- 01_15_dosenordre/DosEnOrdre.java
+++ 01_15_dosenordre/DosEnOrdre.java
@@ -5,7 +5,7 @@ public class DosEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
         int primer = Integer.parseInt(Entrada.readLine());
-        System.out.println("Segón?");
+        System.out.println("Segon?");
         int segon = Integer.parseInt(Entrada.readLine());
 
         if  (primer < segon) {
