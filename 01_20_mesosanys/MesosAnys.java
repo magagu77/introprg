f4115f692cfa5c2249afd2893a9f741a1d3a35a9
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-18 17:16:34 +0200

    Ejercicio 01_20 acabado

---
+++ 01_20_mesosanys/MesosAnys.java
@@ -0,0 +1,19 @@
+//Desenvolupa un programa que demani un mes i un any, i escrigui el mes anterior i el mes següent.
+
+public class MesosAnys {
+    public static void main(String[]args) {
+        
+        System.out.println("Mes?");
+        int mes = Integer.parseInt(Entrada.readLine());
+        System.out.println("Any?");
+        int any = Integer.parseInt(Entrada.readLine());
+        
+        if (mes < 12 ) {
+            System.out.println("Anterior " + (mes - 1) + "/" + any + " i posterior " + (mes + 1) + "/" + any);
+        }
+          else if (mes == 12) {
+              System.out.println("Anterior " + (mes - 1) + "/" + any + " i posterior 1/" + (any + 1));
+          }
+    }
+}
+     
