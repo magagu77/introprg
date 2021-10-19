63795f1b2b56fff1fd1950ea7b237a6166ba4855
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-19 19:52:51 +0200

    Pasar prgtes

---
+++ 01_24_semafor/Semafor.java
@@ -0,0 +1,18 @@
+//Semafor colors strings
+
+public class Semafor {
+    public static void main(String[] args) {
+        System.out.println("Color?");
+        String color = Entrada.readLine();
+        if (color.equals("groc")) {
+            System.out.println("corre!");
+        } else if (color.equals("verd")) {
+            System.out.println("passa");
+        } else if (color.equals("vermell")) {
+            System.out.println("espera");
+        } else {
+            System.out.println("ves a l’oculista");
+        }
+    }
+}
+          
