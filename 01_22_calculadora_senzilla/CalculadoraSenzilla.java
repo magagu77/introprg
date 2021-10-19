df450efea84003dad02cb45d314be9239f977040
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-19 19:24:29 +0200

    pasar prgtest

--- 01_22_calculadora_senzilla/CalculadoraSenzilla.java
+++ 01_22_calculadora_senzilla/CalculadoraSenzilla.java
@@ -15,7 +15,7 @@ public class CalculadoraSenzilla {
             System.out.println(operand1 + " + " + operand2 + " = " + resultat);
         } else if (operador == '-') {
               double resultat = operand1 - operand2;
-              System.out.println(operand1 + " + " + operand2 + " = " + resultat);
+              System.out.println(operand1 + " - " + operand2 + " = " + resultat);
         } else if (operador == '*') {
               double resultat = operand1 * operand2;
               System.out.println(operand1 + " * " + operand2 + " = " + resultat);
