05dc7e9ec50416061df78b2ee7c685d1ad432b5e
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-21 20:05:10 +0200

    Ejercicio 01_30 acabado

---
+++ 01_30_quantsparells/ComptaParells.java
@@ -0,0 +1,19 @@
+//Programa que cuenta enteros pares hasta que introduces un numero negativo
+
+public class ComptaParells {
+    public static void main(String[] args) {
+        
+        System.out.println("Introdueix un valor");
+        int valor = Integer.parseInt(Entrada.readLine());
+        int numerosParells = 0;
+        
+        while (valor >= 0 ) {
+            System.out.println("Introdueix un valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            if (valor % 2 == 0) {
+            numerosParells = numerosParells + 1;
+            }
+        }
+        System.out.println("Valors parells introduïts: " + numerosParells);
+    }
+}
