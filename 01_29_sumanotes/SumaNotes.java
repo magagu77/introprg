ca1aa81522d03c3de603edcbfda91b5e34d0eec0
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-25 17:12:24 +0200

    intentar pasar prgtest

--- 01_29_sumanotes/SumaNotes.java
+++ 01_29_sumanotes/SumaNotes.java
@@ -3,12 +3,12 @@ public class SumaNotes {
     public static void main (String[] args) {
             
         int suma = 0;
-        System.out.println("Introdueix valor");
+        System.out.println("Introdueix una nota");
         int valor = Integer.parseInt(Entrada.readLine());
             
         while (valor >= 0 && valor <= 100) {
             suma = suma + valor;
-            System.out.println("Introdueix valor");
+            System.out.println("Introdueix una nota");
             valor = Integer.parseInt(Entrada.readLine());
         }
             
