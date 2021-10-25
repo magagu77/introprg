538070f3c51cd56ef9bd36350863c7c1a7c4aab9
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-25 17:17:46 +0200

    intentar pasar prgtest

--- 01_30_quantsparells/ComptaParells.java
+++ 01_30_quantsparells/ComptaParells.java
@@ -7,7 +7,7 @@ public class ComptaParells {
         int valor = Integer.parseInt(Entrada.readLine());
         int numerosParells = 0;
         
-        while (valor >= 0 ) {
+        while (valor > 0 ) {
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
             if (valor % 2 == 0) {
