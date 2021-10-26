018abcf5faf13c37895a42274906f97c819c9101
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-26 19:50:26 +0200

    intentar pasar prgtest

---
+++ 01_33_mitja/NotaMitja.java
@@ -0,0 +1,22 @@
+//Mitjana notes while
+
+public class NotaMitja {
+    public static void main(String[] args) {
+        
+        int nota = 0;
+        int numeroNotes = 0;
+        int sumaTotal = 0;
+        int mitjana = 0;
+        
+        while (nota <= 100 && nota >= 0) {
+            System.out.println("Introdueix una nota");
+            nota = Integer.parseInt(Entrada.readLine());
+            if (nota <= 100 && nota >= 0) {
+                numeroNotes = numeroNotes + 1;
+                sumaTotal = sumaTotal + nota;
+                }
+        }
+        mitjana = sumaTotal / numeroNotes;
+        System.out.println("La mitja de les notes vàlides és " + mitjana) ;
+    }
+}           
