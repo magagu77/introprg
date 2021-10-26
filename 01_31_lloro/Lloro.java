05601140fd801d2cfa5c05f7e267b2dacc243b91
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-26 19:21:59 +0200

    intentar pasar prgtest

--- 01_31_lloro/Lloro.java
+++ 01_31_lloro/Lloro.java
@@ -1,14 +1,22 @@
+//Prgrama del loro repite palabras
+
 public class Lloro {
     public static void main(String[] args) {
     
         System.out.println("El lloro espera paraula:");
         String paraula = Entrada.readLine();
-        System.out.println("El lloro repeteix: " + paraula);        
+        
+        if (!(paraula.isEmpty() || paraula.isBlank())) { 
+            System.out.println("El lloro repeteix: " + paraula);        
+        }
         
         while (!(paraula.isEmpty() || paraula.isBlank())) {
           System.out.println("El lloro espera paraula:"); 
           paraula = Entrada.readLine();
-          System.out.println("El lloro repeteix: " + paraula);       
+          
+          if (!(paraula.isEmpty() || paraula.isBlank())) {
+              System.out.println("El lloro repeteix: " + paraula);       
+          }
         }
         System.out.println("Adéu");
     }  
