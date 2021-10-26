e4ee44921d0105b59acf797156d702a1f3ee707c
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-26 19:13:41 +0200

    intentar pasar prgtest

---
+++ 01_31_lloro/Lloro.java
@@ -0,0 +1,14 @@
+public class Lloro {
+    public static void main(String[] args) {
+    
+        System.out.println("El lloro espera paraula:");
+        String paraula = Entrada.readLine();
+        
+        
+        while (paraula.isEmpty() || paraula.isBlank()) {
+          System.out.println("El lloro repeteix: " + paraula);
+          System.out.println("El lloro espera paraula:"); 
+          paraula = Entrada.readLine();
+        }
+    }
+}        
