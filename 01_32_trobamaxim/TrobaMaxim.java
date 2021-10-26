d687a9e1fe0ff783264f3458ff61c23fc50413eb
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-26 19:36:44 +0200

    Ejercicio 01_32 acabado

--- 01_32_trobamaxim/TrobaMaxim.java
+++ 01_32_trobamaxim/TrobaMaxim.java
@@ -2,14 +2,14 @@
 public class TrobaMaxim {
     public static void main(String[] args) {
     
-        int tmp = 0;
+        int maxim = 0;
         int numero = 0;
         
         while (numero >= 0) {
             System.out.println("Introdueix un valor");
             numero = Integer.parseInt(Entrada.readLine());
-            if (numero > tmp) {
-               tmp = numero;
+            if (numero > maxim) {
+               maxim = numero;
             }
         }
         System.out.println("El màxim és " + tmp);
