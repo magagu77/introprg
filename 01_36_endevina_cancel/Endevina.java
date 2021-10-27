c676309c7b8cd138972afbe60f9a2fcced8c647a
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-27 16:09:03 +0200

    intentar pasar prgtest

--- 01_36_endevina_cancel/Endevina.java
+++ 01_36_endevina_cancel/Endevina.java
@@ -3,27 +3,28 @@
 public class Endevina {
     public static void main(String[] args) {
           
-        int nombrePredefinit = 42;
-        String numero = "0";
-        int nombre = Integer.parseInt(numero);
+        int numeroPredefinit = 42;
+        int numero = 0;
+        String nombre = "a";
         
-        while (numero.isEmpty() || !(nombre == nombrePredefinit)) {
+        while (!(nombre.isEmpty()) || !(numero == numeroPredefinit)) {
             System.out.println("Nombre?");
-            numero = Entrada.readLine();
-            nombre = Integer.parseInt(numero);
-            
-                  if  (nombre <= 0 || nombre > 100)  {
+            nombre = Entrada.readLine();
+            if (!(nombre.isEmpty())) { 
+                numero = Integer.parseInt(nombre);
+                if (numero <= 0 || numero > 100) {
                     System.out.println("Fora de rang");
-                } else if (nombre > nombrePredefinit ) {
+                } else if (numero < numeroPredefinit) {
+                      System.out.println("Massa petit");
+                } else if (numero > numeroPredefinit) {
                       System.out.println("Massa gran");
-                } else if (nombre < nombrePredefinit) {
-                      System.out.println("Massa petit") ;  
-                } 
-        }  
-        if (nombre == nombrePredefinit) {
-            System.out.println("Encertat!");
-        } else if (numero.isEmpty()) {
-              System.out.println("Cancel·lat!");
+                }
+            }
+        }
+        if (nombre.isEmpty()) {
+            System.out.println("Cancel·lat!");
+        } else if (numero == numeroPredefinit) {
+              System.out.println("Encertat!");
         }
     }
-} 
+}            
