124e91efd72e59211e72445f2bfb24cc55224b7a
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-27 15:09:53 +0200

    intentar pasar prgtest

---
+++ 01_35_endevina/Endevina.java
@@ -0,0 +1,25 @@
+//programa que es pensarà un nombre enter entre el 1 i el 100, i reptarà els usuaris a que l’endevini.
+
+public class Endevina {
+    public static void main(String[] args) {
+        
+        int nombre = 0;
+        int nombrePredefinit = 42;
+        
+        while (!(nombre == nombrePredefinit)) {
+            System.out.println("Introdueix un nombre entre el 1 i el 100");
+            nombre = Integer.parseInt(Entrada.readLine());
+                if (nombre < nombrePredefinit) {
+                    System.out.println("És més gran que " + nombre);
+                } else if (nombre > nombrePredefinit) {
+                      System.out.println("És mes petit que " + nombre);
+                } else if (nombre < 0 ) {
+                      System.out.println("Com a mínim 1");
+                } else if (nombre > 100 ) {
+                      System.out.println("Com a màxim 100");  
+                } else {
+                      System.out.println("Has encertat!");
+                }
+        }
+    }
+}  
