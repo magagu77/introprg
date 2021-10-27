0904495876d80550a4b8ccca4a2ef98f6dfc8821
Marc Garcia <marc.garcia0502@gmail.com>
2021-10-27 15:53:31 +0200

    intentar pasar prgtest

--- 01_36_endevina_cancel/Endevina.java
+++ 01_36_endevina_cancel/Endevina.java
@@ -2,19 +2,17 @@
 
 public class Endevina {
     public static void main(String[] args) {
-        
-        int nombre = 0;
+          
         int nombrePredefinit = 42;
         String numero = "a";
+        int nombre = Integer.parseInt(numero);
         
         while (numero.isEmpty() || !(nombre == nombrePredefinit)) {
             System.out.println("Nombre?");
             numero = Entrada.readLine();
-            if (!(numero.isEmpty())) {
-                nombre = Integer.parseInt(numero);
-            }
+            nombre = Integer.parseInt(numero);
             
-                if  (nombre <= 0 || nombre > 100)  {
+                  if  (nombre <= 0 || nombre > 100)  {
                     System.out.println("Fora de rang");
                 } else if (nombre > nombrePredefinit ) {
                       System.out.println("Massa gran");
