defa04424894c41f5c105c4d9356d1191d34e1b5
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-02 20:52:41 +0100

    prgtest

---
+++ 01_41_deu_primers_for/DeuPrimers.java
@@ -0,0 +1,12 @@
+//Ejercicio de mostrar los 10 primeros numeros en pantalla pero con un for
+
+public class DeuPrimers {
+    public static void main(String[] args) {
+        
+        for (int numero = 1;
+             numero <= 10;
+             numero = numero + 1) {
+             System.out.println(numero);
+        }    
+    }
+}        
