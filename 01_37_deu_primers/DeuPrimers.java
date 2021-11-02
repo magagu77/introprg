c143f8c38703a0e8ff8da96a68c2878c1658c99a
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-02 19:19:45 +0100

    intentar pasar prgtest

--- 01_37_deu_primers/DeuPrimers.java
+++ 01_37_deu_primers/DeuPrimers.java
@@ -4,15 +4,12 @@ public class DeuPrimers {
     public static void main(String[] args) {
     
         int numero = 1;
-        boolean numeroDeu = false;
         
-        while (numeroDeu == false) {
+        while (numero == 11) {
             System.out.println(numero);
             numero = numero + 1;       
-        
-            if (numero == 10) {
-                numeroDeu = true;
-            }
+
+            
         }
     }
 }
