50f2b5b1ca0f2f03e02779028a455dcab24c09bc
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-02 20:12:03 +0100

    prgtest

---
+++ 01_38_deu_primers_decreixent/DeuPrimersDecreixent.java
@@ -0,0 +1,12 @@
+//Igual que el ejercicio anterior pero al decreciente
+
+public class DeuPrimersDecreixent {
+    public static void main(String[] args) {
+    
+        int numero =10;
+        while (numero >= 0) {
+        System.out.println(numero);
+        numero = numero -1;
+        }
+    }
+}
