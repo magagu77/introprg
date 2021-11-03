290ad1aeeb875ef8f0716f0d6d1fdaabc118cca6
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-03 15:37:01 +0100

    prgtest

---
+++ 01_45_suma_nombres/SumaInterval.java
@@ -0,0 +1,18 @@
+// Suma todos los numeros de un intervalo con un for
+
+public class SumaInterval {
+    public static void main (String[] args) {
+        
+        System.out.println("inici?");
+        int inici = Integer.parseInt(Entrada.readLine());
+        System.out.println("final?");
+        int fi = Integer.parseInt(Entrada.readLine());
+        int suma = 0;
+        for (int interval = inici;
+             interval <= fi;
+             interval = interval + 1) {
+             suma = suma + interval; 
+        }
+        System.out.println(suma);
+    }
+}        
