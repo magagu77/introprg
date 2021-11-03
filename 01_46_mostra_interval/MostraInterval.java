4c1490b0922d28054d8062e07ab56f70a0fd6f2f
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-03 16:07:07 +0100

    prgtest

---
+++ 01_46_mostra_interval/MostraInterval.java
@@ -0,0 +1,22 @@
+//Mostrar un intervalo de numeros en pantalla con for
+
+public class MostraInterval {
+    public static void main(String[] args) {
+        
+        System.out.println("inici?");
+        int inici = Integer.parseInt(Entrada.readLine());
+        System.out.println("final?");
+        int fi = Integer.parseInt(Entrada.readLine());
+        
+        for (int interval = inici;
+             interval <= fi;
+             interval = interval + 1) {
+             System.out.println(interval);
+        }
+        for (int interval = inici;
+             interval >= fi;
+             interval = interval - 1) {
+             System.out.println(interval);
+        }
+    }
+}             
