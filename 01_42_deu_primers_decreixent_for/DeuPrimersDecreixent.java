7d69290f2f2b1f473b61040d32a2fec901e05168
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-03 14:54:28 +0100

    prgtest

---
+++ 01_42_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,12 @@
+//Aparecen losnumeros del 10 al 1 pero con un for
+
+public class DeuPrimersDecreixent {
+    public static void main(String[] args) {
+    
+        for (int i = 10 ;
+             i > 0;
+             i = i - 1) {
+             System.out.println(i);
+         }
+     }
+}            
