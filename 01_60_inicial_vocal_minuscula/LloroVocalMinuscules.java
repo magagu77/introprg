595e7db8b8b311598d09761d12665c2f0111113f
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-18 19:57:00 +0100

    Ejercicio 01_60 acabado

---
+++ 01_60_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,23 @@
+/* Desenvolupa una nova versió del programa del Lloro que, en aquest cas només
+   repeteixi les paraules que comencen per una vocal en minúscules i s’aturi 
+   quan rebi una cadena en blanc.
+*/
+
+ public class LloroVocalMinuscules {
+ 	public static void main(String[] args) {
+ 		
+ 		String paraula = Entrada.readLine();
+ 		char inici = paraula.charAt(0);
+ 		while (!paraula.isBlank())
+ 			System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+ 			paraula  = Entrada.readLine();
+ 			if (Character.isLowerCase(paraula.charAt(0)) && inici == 'a' || inici == 'e' || 
+             inici == 'i' || inici == 'o' || inici == 'u') {
+ 				System.out.println("El lloro diu: " + paraula);
+ 			} else {
+ 				System.out.println();
+ 			}
+ 		System.out.println("Adéu");
+ 	}
+ 	
+ }  
