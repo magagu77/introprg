6222cb0140ea82ab6266ff7a33fbc6839563dc9f
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-18 17:02:36 +0100

    Ejercicio 01_58 acabado

---
+++ 01_58_informecaracter/InformeCaracter.java
@@ -0,0 +1,27 @@
+/* Desenvolupa un programa que demani un text i, respecte el primer caràcter,
+   composi un petit informe amb el resultat de les funcions dels caracters
+*/
+
+public class InformeCaracter {
+	public static void main(String[] args) {
+		
+
+		System.out.println("Carácter?");
+		String paraula = Entrada.readLine();
+		char lletra = paraula.charAt(0);
+		
+		System.out.println("Character.getName('" + lletra + "'): " + Character.getName(lletra));
+		System.out.println("Character.isDigit('" + lletra + "'): " + Character.isDigit(lletra));
+		System.out.println("Character.isJavaIdentifierStart('" + lletra + "'): " + Character.isJavaIdentifierStart(lletra));
+		System.out.println("Character.isJavaIdentifierPart('" + lletra + "'): " + Character.isJavaIdentifierPart(lletra));
+		System.out.println("Character.isLetter('" + lletra + "'): " + Character.isLetter(lletra));
+		System.out.println("Character.isLowerCase('" + lletra + "'): " + Character.isLowerCase(lletra));
+		System.out.println("Character.isUpperCase('" + lletra + "'): " + Character.isUpperCase(lletra));
+		System.out.println("Character.isWhiteSpace('" + lletra + "'): " + Character.isWhitespace(lletra));
+		if (Character.isWhitespace(lletra)) {
+			System.out.println("Caràcter buit");
+		}
+		System.out.println("Character.toLowerCase('" + lletra + "'): " + Character.toLowerCase(lletra));
+		System.out.println("Character.toUpperCase('" + lletra + "'): " + Character.toUpperCase(lletra));
+	}
+}   
