3281171ab38d471abd4754ef301c7f9582beea6f
prgtest <marc.garcia0502@gmail.com>
2021-11-18 17:06:29 +0100

    prgtest autocommit #5 01_58: Ejercicio 01_58 acabado

--- 01_58_informecaracter/InformeCaracter.java
+++ 01_58_informecaracter/InformeCaracter.java
@@ -11,17 +11,18 @@ public class InformeCaracter {
 		char lletra = paraula.charAt(0);
 		if (paraula.isBlank()){
 			System.out.println();
-		}
-		System.out.println("Character.getName('" + lletra + "'): " + Character.getName(lletra));
-		System.out.println("Character.isDigit('" + lletra + "'): " + Character.isDigit(lletra));
-		System.out.println("Character.isJavaIdentifierStart('" + lletra + "'): " + Character.isJavaIdentifierStart(lletra));
-		System.out.println("Character.isJavaIdentifierPart('" + lletra + "'): " + Character.isJavaIdentifierPart(lletra));
-		System.out.println("Character.isLetter('" + lletra + "'): " + Character.isLetter(lletra));
-		System.out.println("Character.isLowerCase('" + lletra + "'): " + Character.isLowerCase(lletra));
-		System.out.println("Character.isUpperCase('" + lletra + "'): " + Character.isUpperCase(lletra));
-		System.out.println("Character.isWhitespace('" + lletra + "'): " + Character.isWhitespace(lletra));
-		System.out.println("Character.toLowerCase('" + lletra + "'): " + Character.toLowerCase(lletra));
-		System.out.println("Character.toUpperCase('" + lletra + "'): " + Character.toUpperCase(lletra));
+		} else {
+			System.out.println("Character.getName('" + lletra + "'): " + Character.getName(lletra));
+			System.out.println("Character.isDigit('" + lletra + "'): " + Character.isDigit(lletra));
+			System.out.println("Character.isJavaIdentifierStart('" + lletra + "'): " + Character.isJavaIdentifierStart(lletra));
+			System.out.println("Character.isJavaIdentifierPart('" + lletra + "'): " + Character.isJavaIdentifierPart(lletra));
+			System.out.println("Character.isLetter('" + lletra + "'): " + Character.isLetter(lletra));
+			System.out.println("Character.isLowerCase('" + lletra + "'): " + Character.isLowerCase(lletra));
+			System.out.println("Character.isUpperCase('" + lletra + "'): " + Character.isUpperCase(lletra));
+			System.out.println("Character.isWhitespace('" + lletra + "'): " + Character.isWhitespace(lletra));
+			System.out.println("Character.toLowerCase('" + lletra + "'): " + Character.toLowerCase(lletra));
+			System.out.println("Character.toUpperCase('" + lletra + "'): " + Character.toUpperCase(lletra));
+		} 
 		if (Character.isWhitespace(lletra)) {
 			System.out.println("Caràcter buit");
 		}	
