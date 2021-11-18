8e7a146bf1abd53f31ce80b56018ba0075338338
prgtest <marc.garcia0502@gmail.com>
2021-11-18 19:04:35 +0100

    prgtest autocommit #6 01_59: Ejercicio 01_59 acabat

--- 01_59_manipulant_strings/ManipulaString.java
+++ 01_59_manipulant_strings/ManipulaString.java
@@ -11,7 +11,7 @@ public class ManipulaString {
 		System.out.println("La segona lletra és '" + paraula.charAt(1) + "'");
 		System.out.println("La darrera lletra és '" + paraula.charAt((paraula.length() - 1)) + "'");
 		System.out.println("La penúltima lletra és '" + paraula.charAt((paraula.length() - 2)) + "'");
-		if (paraula.length() < 3){
+		if (paraula.length() > 3){
 			System.out.println("La paraula en majúscules és \"" + paraula.toUpperCase() + "\"");
 			System.out.println("La composició de quatre primeres és \"" + Character.toUpperCase(paraula.charAt(0)) + Character.toLowerCase(paraula.charAt(1)) + Character.toLowerCase(paraula.charAt(2)) + Character.toLowerCase(paraula.charAt(3)) + "\"");
 			System.out.println("La composició de quatre és \"" + Character.toLowerCase(paraula.charAt((paraula.length() - 1))) + Character.toUpperCase(paraula.charAt(0)) + Character.toLowerCase(paraula.charAt(1)) + Character.toLowerCase(paraula.charAt(2)) + "\"");
