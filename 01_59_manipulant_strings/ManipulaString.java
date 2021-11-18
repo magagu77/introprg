47b68e1ed0f02db616655d88fd7525e8070d5b02
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-18 19:00:26 +0100

    Ejercicio 01_59 acabat

---
+++ 01_59_manipulant_strings/ManipulaString.java
@@ -0,0 +1,19 @@
+/* Completa el següent programa, de manera que demani un text i mostri 
+   els aspectes demanats als comentaris.
+*/
+
+public class ManipulaString {
+	public static void main(String[] args) {
+			
+			System.out.println("Paraula?");
+			String paraula = Entrada.readLine();
+			System.out.println("La longitud és '" + paraula.length() + "'");
+			System.out.println("La segona lletra és '" + paraula.charAt(1) + "'");
+			System.out.println("La derrera lletra és '" + paraula.charAt((paraula.length() - 1)) + "'");
+			System.out.println("La penúltima és '" + paraula.charAt((paraula.length() - 2)) + "'");
+			System.out.println("La paraula en majúscules és \"" + paraula.toUpperCase() + "\"");
+			System.out.println("La composició de quatre primeres és \"" + Character.toUpperCase(paraula.charAt(0)) + Character.toLowerCase(paraula.charAt(1)) + Character.toLowerCase(paraula.charAt(2)) + Character.toLowerCase(paraula.charAt(3)) + "\"");
+			System.out.println("La composició de quatre és \"" + Character.toLowerCase(paraula.charAt((paraula.length() - 1))) + Character.toUpperCase(paraula.charAt(0)) + Character.toLowerCase(paraula.charAt(2)) + Character.toLowerCase(paraula.charAt(3)) + "\"");
+			System.out.println("La composició de quatre corregint majúscules és \"" + Character.toUpperCase(paraula.charAt((paraula.length() - 1))) + Character.toLowerCase(paraula.charAt(0)) + Character.toLowerCase(paraula.charAt(2)) + Character.toLowerCase(paraula.charAt(3)) + "\"");
+		}	
+}   
