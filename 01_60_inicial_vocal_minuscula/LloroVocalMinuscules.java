d83a47f8dd3c09d66ede7611da99cc38eec33a28
prgtest <marc.garcia0502@gmail.com>
2021-11-22 16:08:34 +0100

    prgtest autocommit #1 01_60: Ejercicio 01_60 acabdao

--- 01_60_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 01_60_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -8,10 +8,13 @@
  		
  		System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
       String paraula  = Entrada.readLine();
+      char inici = paraula.charAt(0);
  		while (!(paraula.isBlank())) {
  			System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
  			paraula  = Entrada.readLine();
-         char inici = paraula.charAt(0);
+         if (!paraula.isBlank()) {
+            inici = paraula.charAt(0);
+         }   
  			if (Character.isLowerCase(paraula.charAt(0)) && inici == 'a' || inici == 'e' || 
              inici == 'i' || inici == 'o' || inici == 'u') {
  				System.out.println("El lloro diu: " + paraula);
