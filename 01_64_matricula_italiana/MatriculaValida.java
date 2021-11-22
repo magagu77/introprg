b1f8970c2e22fc7c7b94b7ed4d86bc3b7cc57651
prgtest <marc.garcia0502@gmail.com>
2021-11-22 18:20:19 +0100

    prgtest autocommit #7 01_64: Ejercicio 01_64 acabado

--- 01_64_matricula_italiana/MatriculaValida.java
+++ 01_64_matricula_italiana/MatriculaValida.java
@@ -17,6 +17,10 @@ public class MatriculaValida {
 					if (Character.isLetter(matricula.charAt(end)) && Character.isUpperCase(matricula.charAt(end)) && end >= 'A' && end <= 'Z')
 						valida = "És una matrícula italiana vàlida";
 				}
+				else {
+					valida = "No és una matrícula italiana vàlida";
+					break;
+				}
 				if (end >= 2 && end <= 4) { 
 					if (Character.isDigit(matricula.charAt(end))) {
 						valida = "És una matrícula italiana vàlida";
