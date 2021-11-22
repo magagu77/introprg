30d5516dee7fbd0d6a22aeb3e14c5c892180f850
prgtest <marc.garcia0502@gmail.com>
2021-11-22 17:19:56 +0100

    prgtest autocommit #1 01_64: Ejercicio 01_64 acabado

--- 01_64_matricula_italiana/MatriculaValida.java
+++ 01_64_matricula_italiana/MatriculaValida.java
@@ -10,21 +10,23 @@ public class MatriculaValida {
 		
 		System.out.println("Introduïu una matrícula");
 		String matricula = Entrada.readLine();
-		char lletra1 = matricula.charAt(0);
-		char lletra2 = matricula.charAt(1);
-		if (Character.isLetter(matricula.charAt(0)) && 
-			Character.isLetter(matricula.charAt(1))) {
-			if (!(lletra1 == 'O' || lletra1 == 'I' || lletra1 == 'Q' || lletra1 == 'U')) {
-				if (!(lletra2 == 'O' || lletra2 == 'I' || lletra2 == 'Q' || lletra2 == 'U'))
-					if (Character.isDigit(matricula.charAt(2)) && 
-						Character.isDigit(matricula.charAt(3)) &&
-						Character.isDigit(matricula.charAt(4))) {
-						if (Character.isLetter(matricula.charAt(5)) && 
-							Character.isLetter(matricula.charAt(6))) {
-							System.out.println("És una matrícula italiana vàlida");
+		if (matricula.length() == 7) {	
+			char lletra1 = matricula.charAt(0);
+			char lletra2 = matricula.charAt(1);
+			if (Character.isLetter(matricula.charAt(0)) && 
+				Character.isLetter(matricula.charAt(1))) {
+				if (!(lletra1 == 'O' || lletra1 == 'I' || lletra1 == 'Q' || lletra1 == 'U')) {
+					if (!(lletra2 == 'O' || lletra2 == 'I' || lletra2 == 'Q' || lletra2 == 'U'))
+						if (Character.isDigit(matricula.charAt(2)) && 
+							Character.isDigit(matricula.charAt(3)) &&
+							Character.isDigit(matricula.charAt(4))) {
+							if (Character.isLetter(matricula.charAt(5)) && 
+								Character.isLetter(matricula.charAt(6))) {
+								System.out.println("És una matrícula italiana vàlida");
+							}
 						}
-					}
-			}
+				}
+			}	
 		} else {
 			System.out.println("No és una matrícula italiana vàlida");
 		}
