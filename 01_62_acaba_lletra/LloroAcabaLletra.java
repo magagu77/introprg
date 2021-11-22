32e9d1c2412cac0dcdf979b51d00d651dcd52d9c
prgtest <marc.garcia0502@gmail.com>
2021-11-22 16:49:45 +0100

    prgtest autocommit #6 01_62: Ejercicio 01_62 acabado

--- 01_62_acaba_lletra/LloroAcabaLletra.java
+++ 01_62_acaba_lletra/LloroAcabaLletra.java
@@ -10,12 +10,10 @@ public class LloroAcabaLletra {
 		String paraula = Entrada.readLine();
 		while (!paraula.isBlank()) {
 			char end = paraula.charAt(paraula.length() - 1);
-			if (!Character.isLetter(end)) {
-				end = paraula.charAt(paraula.length() - 2);
-			}
-			if (!(end == 'a' || end == 'e' || end == 'i' || end == 'o' || end == 'u' || 
+			
+			if (!(Character.isLetter(end) || end == 'a' || end == 'e' || end == 'i' || end == 'o' || end == 'u' || 
 				  end == 'A' || end == 'E' || end == 'I' || end == 'O' || end == 'U')) {
-				System.out.println("El lloro diu: " + paraula);
+				String.format("El lloro diu: " + paraula);
 			} else {
 				System.out.println();
 			}
