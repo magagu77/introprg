59e8638fbde323ccb97467bf5cbd3b26ad49f1e4
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-23 16:56:24 +0100

    Ejercicio 01_69 acabado

---
+++ 01_69_mostra_interval_string/MostraInterval.java
@@ -0,0 +1,35 @@
+/* Desenvolupa un programa que demani un text i dos valors enters, i que mostri
+   tots els caràcters que hi ha entre el primer i el segon, en l’ordre marcat
+   per l’entrada.
+   En cas que els valors enters quedin fora de les posicions del text introduït,
+   el programa mostrarà tot el que es pugui sense queixar-se.
+*/
+
+public class MostraInterval {
+	public static void main(String[] args) {
+		System.out.println("text?");
+		String text = Entrada.readLine();
+		System.out.println("inici?");
+		int inici = Integer.parseInt(Entrada.readLine());
+		System.out.println("final?");
+		int end = Integer. parseInt(Entrada.readLine());
+		if (inici < end) {
+			for (int i = inici; i <= end; i++) {
+				if (i >= text.length()) {
+					break;
+				} else {
+				System.out.println(Character.toLowerCase(text.charAt(i)));
+				}
+			}
+		}
+		if (end < inici) {
+			for (int i = inici; i >= end; i--) {
+				if (i <= -1) {
+					break;
+				} else {
+					System.out.println(Character.toLowerCase(text.charAt(i)));
+				}
+			}
+		}
+	}
+}   
