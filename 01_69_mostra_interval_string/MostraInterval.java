e1e148fdaa919fd240e1299a9727b0e40d74cdb8
prgtest <marc.garcia0502@gmail.com>
2021-11-23 17:19:35 +0100

    prgtest autocommit #8 01_69: Ejercicio 01_69 acabado

--- 01_69_mostra_interval_string/MostraInterval.java
+++ 01_69_mostra_interval_string/MostraInterval.java
@@ -13,36 +13,38 @@ public class MostraInterval {
 		int inici = Integer.parseInt(Entrada.readLine());
 		System.out.println("final?");
 		int end = Integer. parseInt(Entrada.readLine());
-		if (inici < 0) {
-			inici = 0;
-		}
-		if (end < 0) {
-			end = 0;
-		}	
-		if (inici > text.length() - 1) {
-			inici = text.length() - 1;
-		}	
-		if (end > text.length() - 1) {
-			end = text.length() - 1;
-		}
-		if (inici <= end) {
-			for (int i = inici; i <= end; i++) {
-				if (i >= text.length()) {
-					break;
-				} else {
-					System.out.println(text.charAt(i));
+		if (inici >= 0 || end >= 0) {
+			if (inici < 0) {
+				inici = 0;
+			}
+			if (end < 0) {
+				end = 0;
+			}	
+			if (inici > text.length() - 1) {
+				inici = text.length() - 1;
+			}	
+			if (end > text.length() - 1) {
+				end = text.length() - 1;
+			}
+			if (inici <= end) {
+				for (int i = inici; i <= end; i++) {
+					if (i >= text.length()) {
+						break;
+					} else {
+						System.out.println(text.charAt(i));
+					}
 				}
 			}
-		}
-		if (end < inici) {
-			for (int i = inici; i >= end; i--) {
-				if (i <= -1) {
-					break;
-				} else {
-					System.out.println(text.charAt(i));
+			if (end < inici) {
+				for (int i = inici; i >= end; i--) {
+					if (i <= -1) {
+						break;
+					} else {
+						System.out.println(text.charAt(i));
+					}
 				}
 			}
-		}
+		}	
 	}	
 }
    
