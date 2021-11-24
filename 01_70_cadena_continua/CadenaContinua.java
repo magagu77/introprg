2fffaef4a1bb7f7718a68832ed5f8451479d9446
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-24 16:37:10 +0100

    Ejercicio 01_70 acabado

---
+++ 01_70_cadena_continua/CadenaContinua.java
@@ -0,0 +1,31 @@
+/*Desenvolupa un programa que demani un text i un nombre enter, i mostri tants
+  caràcters del text com indiqui el nombre enters, començant pel primer. En cas
+  que en faltin, el programa tornarà a mostrar el text a partir del primer caràcter
+  fins que hagi aconseguit tots els caràcters demanats.
+*/
+
+public class CadenaContinua {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		if (text.isBlank()) {
+			return;
+		}
+		System.out.println("Nombre?");
+		String nombre = Entrada.readLine();
+		if (nombre.isBlank()) {
+			return;
+		}
+		int numero = Integer.parseInt(nombre);
+		int resultat;
+		for(int i = 0; i < numero; i++) {
+			if (i > text.length() - 1) {
+				resultat = i % (text.length() - 1);
+				System.out.print(text.charAt(resultat));
+			} else {
+				System.out.print(text.charAt(i));
+			}
+		}
+		System.out.println();
+	}
+}
