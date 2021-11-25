8cd0ebc929f30bdabc4ac025884b4a240b0062fa
prgtest <marc.garcia0502@gmail.com>
2021-11-25 16:55:02 +0100

    prgtest autocommit #9 01_70: Ejercicio 01_70 acabado

--- 01_70_cadena_continua/CadenaContinua.java
+++ 01_70_cadena_continua/CadenaContinua.java
@@ -14,22 +14,13 @@ public class CadenaContinua {
 		}
 		System.out.println("Nombre?");
 		String nombre = Entrada.readLine();
-		if (nombre.isBlank() || nombre.equals("0")) {
+		if (nombre.isBlank()) {
 			return;
 		}
 		int numero = Integer.parseInt(nombre);
-		int divisio = numero / (text.length());
-		if (numero <= text.length()){
-			for(int i = 0; i < numero; i++) {
-				System.out.print(text.charAt(i));
-			}
-		}	
-		if (divisio > 1) {
-			for (int d = 1; d < divisio; d++) {
-				for(int p = 0; p < (text.length() - 1); p++) {
-					System.out.print(text.charAt(p));
-				}
-			}
-		}
+		 	for (int i = 0; i < numero; i++) {
+		 		System.out.print(text.charAt(i));
+		 	}
+		
 	}
 }
