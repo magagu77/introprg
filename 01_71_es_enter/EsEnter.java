11e6d477bd7e6a8000364f9869dc0456dbd8d41e
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-25 17:12:10 +0100

    Ejercicio 01_71 acabado

---
+++ 01_71_es_enter/EsEnter.java
@@ -0,0 +1,23 @@
+/*Desenvolupa un programa que vagi demanant texts fins rebre la cadena buida.
+  Per cada text no buit que rebi, indicarà si correspon o no a un nombre enter 
+  escrit amb dígits.
+*/
+public class EsEnter {
+	public static void main(String[] args) {
+
+
+		String text = "a";
+		while (!text.isEmpty())
+		System.out.println("Introdueix texts (enter sol per finalitzar)");
+		text = Entrada.readLine();
+		if (text.isEmpty()) {
+			System.out.println("Adéu");
+			return;
+		}
+		for (int i = 0; i <= text.length(); i++) {
+			if (Character.isDigit(text.charAt(i))) {
+				System.out.println("És enter");
+			}
+		}
+	}
+}
