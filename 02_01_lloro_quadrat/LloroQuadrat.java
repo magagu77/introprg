a3cdbad809b7020781387b5ad35f4facaa7d2f1b
Marc Garcia <marc.garcia0502@gmail.com>
2021-11-30 19:25:26 +0100

    Ejercicio 02_01 acabado

---
+++ 02_01_lloro_quadrat/LloroQuadrat.java
@@ -0,0 +1,35 @@
+/* Desenvolupa una versió del lloro que, com és tradicional, vagi demanant 
+   texts i els repeteixi fins que arribi un text en blanc o buit.
+
+   En aquesta versió, no obstant, si el text que rep és "dibuixa quadrat" o 
+   "dibuixa rectangle", en comptes de repetir el text, dibuixarà el quadrat
+   o el rectangle corresponent.
+*/
+
+ public class LloroQuadrat {
+ 	public static void main(String[] args) {
+ 		lloro();
+ 	}
+ 		public static void lloro() {
+ 			String text = "A";
+ 			while(!text.isBlank()) {
+ 				System.out.println("El lloro espera paraula:");
+ 				text = Entrada.readLine();
+ 				if (text.equals("dibuixa quadrat")) {
+ 					dibuixaQuadrat();
+ 				} else if (!text.isBlank()) {
+ 					System.out.println("El lloro repeteix: " + text);
+ 				}
+ 			}
+ 		}
+ 		public static void dibuixaQuadrat() {
+ 			for(int l = 1; l <= 5; l++) {
+ 				for(int c = 1; c <= 5; c++) {
+ 					System.out.print("X");
+ 				}
+ 				System.out.println();
+ 			}
+ 		}
+
+ 	
+ } 
