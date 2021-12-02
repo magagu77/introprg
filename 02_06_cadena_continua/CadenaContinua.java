e7fe36b38110ddb0dc94e19b36ceea7272cff90f
Marc Garcia <marc.garcia0502@gmail.com>
2021-12-02 19:19:55 +0100

    Ejercicio 02_06 acabd0

---
+++ 02_06_cadena_continua/CadenaContinua.java
@@ -0,0 +1,32 @@
+/* Desenvolupa una nova versió de l’exercici que mostrava una cadena contínua.
+   La nova versió tindrà la mateixa sortida que l’original però el codi del
+   programa serà més modular. En concret, el mòdul main() se n’encarregarà 
+   d’obtenir les dades d’entrada, i cridarà un nou mòdul anomenat mostra
+   CadenaContinua() que serà qui realitzi realment la feina de mostrar el resultat.
+*/
+public class CadenaContinua {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		if (text.isBlank()) {
+			System.out.println("error");
+			return;
+		}
+		System.out.println("Nombre?");
+		String nombre = Entrada.readLine();
+		if (nombre.isBlank()) {
+			return;
+		}
+		cadena(text, nombre);
+	}
+	public static void cadena(String text, String nombre) {
+		int numero = Integer.parseInt(nombre);
+		for (int i = 0; i < numero; i++) {
+		 	if (numero <= text.length()) {
+		 		System.out.print(text.charAt(i));
+		 	} else if (numero > text.length()) {
+		 		System.out.print(text.charAt(i % text.length()));
+		 	}
+		}
+	}
+}   
