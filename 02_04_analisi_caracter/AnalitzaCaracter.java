c42d7c92f3d71a05e85757e8e1d88f469e90ca9b
Marc Garcia <marc.garcia0502@gmail.com>
2021-12-02 18:16:24 +0100

    Ejercici0 02_04 acabado

---
+++ 02_04_analisi_caracter/AnalitzaCaracter.java
@@ -0,0 +1,40 @@
+/* Desenvolupa una nova versió de l’exercici que analitzava un caràcter.
+   La nova versió tindrà el mateix comportament que l’original però el codi
+   serà diferent:
+   el mòdul main() se n’encarregarà d’obtenir les dades d’entrada, comprovarà
+   que siguin vàlides, mostrarà els errors corresponents, i n’extraurà el caràcter
+   que finalment s’haurà d’analitzar.
+   Aquest caràcter a analitzar se li passarà al mòdul analitzaCaracter() que serà
+   qui se n’encarregui de realitzar l’anàlisi.
+*/
+public class AnalitzaCaracter {
+	public static void main(String[] args) {
+	System.out.println("Text?");
+	String text = Entrada.readLine();
+	System.out.println("Posició?");
+	int posicio = Integer.parseInt(Entrada.readLine());
+	char caracter = text.charAt(posicio);
+	analitzaCaracter(caracter, posicio, text );	
+	}
+	public static void analitzaCaracter(char caracter, int posicio, String text){
+		if (posicio >= 0 && posicio < text.length()) {
+			if (Character.isLetter(caracter)) {
+				System.out.println("'" + caracter + "' és una lletra");
+			} else if (Character.isDigit(caracter)) {
+				System.out.println("'" + caracter + "' és un nombre");
+			} else {
+				System.out.println("'" + text.charAt(posicio) + "' és una altra cosa");
+			}	
+		} else if (posicio < 0 && posicio + text.length() >= 0) {
+			if (Character.isLetter(caracter)) {
+				System.out.println("'" + caracter + "' és una lletra");
+			} else if (Character.isDigit(caracter)) {
+				System.out.println("'" + caracter + "' és un nombre");
+			} else {
+				System.out.println("'" + caracter + "' és una altra cosa");
+			}
+		} else {
+			System.out.println("Fora de rang");
+		}	
+	}
+}
