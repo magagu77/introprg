15acac5e7f43e314f0c1ae0db585157e24281584
Marc Garcia <marc.garcia0502@gmail.com>
2021-12-02 17:04:13 +0100

    Ejercicio 02_03 acabado

---
+++ 02_03_quadrat_reparametritzat/Quadrat.java
@@ -0,0 +1,23 @@
+/*Farem ara una nova versió del quadrat parametritzat. En aquesta ocasió, 
+  el programa serà capaç de mostrar un caràcter diferent de X a partir del
+  que main() rebi a args[1]
+
+  Per simplificar, pressuposarem també que sempre ens donen aquest segon 
+  argument i que ens quedarem amb el primer caràcter.
+*/
+ public class Quadrat {
+ 	public static void main(String[] args) {
+ 		int numero = Integer.parseInt(args[0]);
+    char caracter = args[1].charAt(0);
+    dibuixaQuadrat(numero, caracter);
+ 	}
+
+ 	public static void dibuixaQuadrat(int numero, char caracter) {
+ 		for(int l = 1; l <= numero; l++) {
+ 				for(int c = 1; c <= numero; c++) {
+ 					System.out.print(" " + caracter);
+ 				}
+ 				System.out.println();
+ 			}
+ 	}
+ }
