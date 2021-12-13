0f441feadd82dbbdd573a20c8710b52f842c7aba
Marc Garcia <marc.garcia0502@gmail.com>
2021-12-13 15:41:21 +0100

    Ejercicio 02_08 acabado

---
+++ 02_08_triangle_invertit_lletres/TriangleLletresInvertit.java
@@ -0,0 +1,32 @@
+/*  Desenvolupa una nova versió de l’exercici que dibuixava un triangle de lletres 
+	invertit.
+
+	La nova versió tindrà la mateixa sortida que l’original però el codi del 
+	programa serà més modular. En concret, el mòdul main() se n’encarregarà 
+	d’obtenir les dades d’entrada, i cridarà un nou mòdul anomenat dibuixaTriangle()
+    que serà qui realitzi realment la feina de mostrar el triangle. De fet, aquest 
+    es recolzarà en un altre anomenat dibuixaLinia() que serà qui realment faci 
+    la feina de dibuixar cada línia del triangle.
+*/
+ public class TriangleLletresInvertit {
+ 	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		dibuixaTriangleInvertit(text);
+	}
+	public static void dibuixaTriangleInvertit(String text) {
+		for (int linia = text.length(); linia >= 0; linia--) {
+			dibuixaLiniaInvertida(text, linia);
+			System.out.println();
+		}
+
+	}
+	public static void dibuixaLiniaInvertida(String text, int linia) {
+		for (int c = 0; c < linia; c++) {
+			if (c + 1 == linia) {
+				System.out.print(text.charAt(c));
+			} else
+				System.out.print(text.charAt(c) + ", ");
+		}
+	}
+}	
