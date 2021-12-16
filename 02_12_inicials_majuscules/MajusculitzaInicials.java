c478d6492583ce71882e8b8816bea1005040b4a8
prgtest <marc.garcia0502@gmail.com>
2021-12-16 19:53:42 +0100

    prgtest autocommit #1 02_12: Ejercicio 02_12 acabado

--- 02_12_inicials_majuscules/MajusculitzaInicials.java
+++ 02_12_inicials_majuscules/MajusculitzaInicials.java
@@ -17,7 +17,7 @@
  				System.out.print(Character.toUpperCase(text.charAt(c)));
  			}
  			else if (Character.isLetter(text.charAt(c)) && 
- 				text.charAt(c - 1) == ' ') {
+ 				text.charAt(c - 1) == ' ' || text.charAt(c - 1) == '-') {
  				System.out.print(Character.toUpperCase(text.charAt(c)));
  			} else {
  				System.out.print(Character.toLowerCase(text.charAt(c)));
