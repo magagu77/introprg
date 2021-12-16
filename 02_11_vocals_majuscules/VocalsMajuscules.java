67031b8d6cb7cf8338686cda5116c8947a2562fa
Marc Garcia <marc.garcia0502@gmail.com>
2021-12-16 19:36:50 +0100

    Ejercicio 02_11 acabado

---
+++ 02_11_vocals_majuscules/VocalsMajuscules.java
@@ -0,0 +1,25 @@
+/* Desenvolupa un programa que demani un text i mostri totes les lletres en 
+   minúscules excepte les vocals, que hauran d’estar en majúscules.
+
+   Per simplicitat, considerarem només les vocals a, e, i, o i u.
+*/
+ public class VocalsMajuscules {
+ 	public static void main(String[] args) {
+ 		System.out.println("Text?");
+ 		String text = Entrada.readLine();
+ 		majusculitzaVocals(text);
+ 	}
+ 	public static void majusculitzaVocals(String text) {
+ 		//Recorrido del texto comprovando caracteres
+ 		for (int i = 0; i < text.length(); i++) {
+ 			if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || 
+ 				text.charAt(i) == 'i' || text.charAt(i) == 'o' ||
+ 				text.charAt(i) == 'u') {
+ 				System.out.print(Character.toUpperCase(text.charAt(i)));
+ 			} else {
+ 				System.out.print(text.charAt(i));
+ 			}
+ 		}
+ 		System.out.println();
+ 	}
+ }
