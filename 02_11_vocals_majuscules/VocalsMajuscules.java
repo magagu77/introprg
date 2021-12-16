338e7cda315d33322c3cd89d118fe3f2077671ae
prgtest <marc.garcia0502@gmail.com>
2021-12-16 19:39:40 +0100

    prgtest autocommit #2 02_11: Ejercicio 02_11 acabado

--- 02_11_vocals_majuscules/VocalsMajuscules.java
+++ 02_11_vocals_majuscules/VocalsMajuscules.java
@@ -14,7 +14,9 @@
  		for (int i = 0; i < text.length(); i++) {
  			if (text.charAt(i) == 'a' || text.charAt(i) == 'e' || 
  				text.charAt(i) == 'i' || text.charAt(i) == 'o' ||
- 				text.charAt(i) == 'u') {
+ 				text.charAt(i) == 'u' || text.charAt(i) == 'A' ||
+ 				text.charAt(i) == 'E' || text.charAt(i) == 'I' || 
+ 				text.charAt(i) == 'O' || text.charAt(i) == 'U') {
  				System.out.print(Character.toUpperCase(text.charAt(i)));
  			} else {
  				System.out.print(Character.toLowerCase(text.charAt(i)));
