aae0012ee4faa757bf904997e59c684dc326b45d
prgtest <marc.garcia0502@gmail.com>
2022-01-10 17:27:04 +0100

    prgtest autocommit #1 02_30: Ejercicio 02_30 acabado

--- 02_30_enters_entre_comes_v1/EntersEntreComes.java
+++ 02_30_enters_entre_comes_v1/EntersEntreComes.java
@@ -5,10 +5,10 @@ public class EntersEntreComes {
 	public static void main(String[] args) {
 		int[] numeros;
 		numeros = new int[3];
-		numeros[1] = 1;
-		numeros[2] = 2;
-		numeros[3] = 3;
-		System.out.print(numeros[1]);
+		numeros[0] = 1;
+		numeros[1] = 2;
+		numeros[2] = 3;
+		System.out.print(numeros[0]);
 		for (int i=1;i<numeros.length;i++) {
 			System.out.print(", " +numeros[i]);
 		}
