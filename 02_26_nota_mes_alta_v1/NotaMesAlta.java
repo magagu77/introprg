e97f13b67d0d55a8913daecb1cf6a0e2925a7d3a
prgtest <marc.garcia0502@gmail.com>
2022-01-10 16:52:01 +0100

    prgtest autocommit #1 02_15: Entendiendo el ejercicio 02_15

--- 02_26_nota_mes_alta_v1/NotaMesAlta.java
+++ 02_26_nota_mes_alta_v1/NotaMesAlta.java
@@ -5,17 +5,17 @@
 public class NotaMesAlta {
 	public static void main(String[] args) {
 		int nota = 1;
-		int numNota = 0;
+		int numNota = 1;
 		int notaFinal = 0;
-		for (int i = 1; i < 0; i++) {
+		while (nota > 0) {
 			System.out.println("Introdueix les notes (-1 per finalitzar)");
 			nota = Integer.parseInt(Entrada.readLine());
-			if (nota >= 0) {
+			if (nota >= 0 && nota <= 10) {
 				numNota++;
-			}
-			if (notaFinal < nota) {
+				if (notaFinal < nota) {
 				notaFinal = nota;
-			}
+				}
+			} 	
 		}
 		if (numNota > 2 ) {
 			System.out.println("La nota més alta és " + notaFinal);
