6db61e79371a50661faa8ea2e8fee9877f0ed96b
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-10 17:12:04 +0100

    Ejercicio 02_27 acabado

---
+++ 02_27_nota_mes_alta_v2/NotaMesAlta.java
@@ -0,0 +1,34 @@
+/*  Desenvolupa una nova versió del programa anterior (02_26). Aquest cop, 
+	el programa, a banda de mostrar la nota màxima, indicarà també les notes
+	introduïdes.
+*/
+public class NotaMesAlta {
+	public static void main(String[] args) {
+		int nota = 1;
+		int numNota = 1;
+		int notaFinal = 0;
+		int notaProvisional = 0;
+		String text = ("La nota més alta és "+notaFinal+" de les introduïdes: ");
+		while (nota > 0) {
+			System.out.println("Introdueix les notes (-1 per finalitzar)");
+			nota = Integer.parseInt(Entrada.readLine());
+			if (nota >= 0 && nota <= 10) {
+				numNota++;
+				notaProvisional = nota; 
+				if (notaFinal < nota) {
+				notaFinal = nota;
+				}
+			} 
+			if (numNota >= 0) {
+				text = text + notaProvisional + ", "; 	
+			} else {
+				text = text + "i " + notaProvisional;
+			}
+		}
+		if (numNota >= 2 ) {
+			System.out.println(text);
+		} else {
+			System.out.println("Com a mínim calen dues notes");
+		}
+	}
+}
