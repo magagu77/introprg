781b5c36cd66ea0a24a4913cdf6156ed7d88bd18
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-15 17:21:03 +0100

    Ejercicio 02_19 acabado

---
+++ 02_19_utilitats_confirmacio/Semafor.java
@@ -0,0 +1,23 @@
+/*  Otra version del programa semafor pero que utiliza un modulo externo para 
+	confirmar si el usuario es mayor de edad
+*/
+public class Semafor {
+	public static void main(String[] args) {
+		System.out.println("Et's major d'edat?");
+		String text = Entrada.readLine();
+		if (UtilitatsConfirmacio.respostaABoolean(text)) {
+			String color = Entrada.readLine();
+	        if (color.equals("groc")) {
+	            System.out.println("corre!");
+	        } else if (color.equals("verd")) {
+	            System.out.println("passa");
+	        } else if (color.equals("vermell")) {
+	            System.out.println("espera");
+	        } else {
+	            System.out.println("ves a l'oculista");
+	        }
+		} else {
+			System.out.println("No pots fer servir aquest el programa sense supervisió");
+		}	
+	}
+}
