ec64c043883c7d1510554d9a169f4cacc2737ea5
prgtest <marc.garcia0502@gmail.com>
2022-01-15 21:26:45 +0100

    prgtest autocommit #1 02_21: Ejercicio 02_21 acabado

--- 02_21_nomes_lletres/UtilString.java
+++ 02_21_nomes_lletres/UtilString.java
@@ -29,10 +29,10 @@ public class UtilString {
 	public static String lletresSeparades(String text) {
 		String lletres = "";
 		for (int i=0;i<text.length(); i++) {
-			if (text.charAt(i) == text.length()-1) {
-				lletres = lletres + text.charAt(i);
-			} else {
+			if (text.charAt(i) > text.length()-2) {
 				lletres = lletres + text.charAt(i) + ", ";
+			} else {
+				lletres = lletres + text.charAt(i);
 			}
 		}
 		return lletres;
