bca68ab7c499f7045c4406931c7f4107e345621e
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-15 17:52:32 +0100

    Ejercicio 02_20 acabado

---
+++ 02_20_es_vocal/UtilString.java
@@ -0,0 +1,16 @@
+/*  Modulo externo a programas que contiene utilidades para strings o char que 
+	no vienen por defecto en java
+*/
+public class UtilString {
+	
+	//Devuelve un valor boolean = true si una letra es vocal
+	public static boolean esVocal(char lletra) {
+		char[] vocals = {'a','à','e','è','é','i','í','ï','o','ó','ò','u','ú','ü'};
+		for (int i=0;i<vocals.length; i++) {
+			if(vocals[i] == lletra) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
