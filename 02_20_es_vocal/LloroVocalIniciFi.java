1c25f423e74616b0af968bb2b9ee3081ab181737
prgtest <marc.garcia0502@gmail.com>
2022-01-15 17:57:07 +0100

    prgtest autocommit #1 02_20: Ejercicio 02_20 acabado

--- 02_20_es_vocal/LloroVocalIniciFi.java
+++ 02_20_es_vocal/LloroVocalIniciFi.java
@@ -8,8 +8,8 @@ public class LloroVocalIniciFi {
 		while(!continuar){
 			System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
 			String text = Entrada.readLine();
-			if (UtilString.esVocal(text.charAt(0)) || 
-				UtilString.esVocal(text.charAt(text.length() - 1))) {
+			if (UtilString.esVocal(text.charAt(0)) || (text.length() > 1 && UtilString.esVocal(text.charAt(text.length() - 1)))) {
+				System.out.println("El lloro diu: " + text);
 
 			} else if(text.isBlank()) {
 				System.out.println("El lloro demana confirmació per finalitzar");
