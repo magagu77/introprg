0e92442ff70f50f4aaa4c8ec26ebbb9a902aface
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-16 09:52:22 +0100

    Ejercicio 02_24 acabado

---
+++ 02_24_es_enter/EsEnter.java
@@ -0,0 +1,19 @@
+/*  Versión modular de EsEnter, que determina si un determinado valor de entrada
+	 es entero o no
+*/
+public class EsEnter {
+	public static void main(String[] args) {
+		System.out.println("Introdueix texts (enter sol per finalitzar)");
+		String text = Entrada.readLine();
+		String valor = "";
+		while(!text.isBlank()) {
+			valor = valor.strip();
+			if (UtilString.esEnter(valor)) {
+				System.out.println("És enter");
+			} else {
+				System.out.println("No és enter");
+			}
+			text = Entrada.readLine();
+		}
+	}
+}
