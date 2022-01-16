e15b2335552df2c92a3040950171e42a180ddd83
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-16 09:07:19 +0100

    Ejercicio 02_23 acabado

---
+++ 02_23_mostra_interval_string/MostraInterval.java
@@ -0,0 +1,14 @@
+/*  Programa que muestra un intervalo de caracteres ordenados en funcion de su
+	posicion en el String y los valores que tu das como inicio y final
+*/
+public class MostraInterval {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		System.out.println("inici?");
+		int inici = Integer.parseInt(Entrada.readLine());
+		System.out.println("final?");
+		int end = Integer.parseInt(Entrada.readLine());
+		System.out.println(UtilString.intervalString(text, inici, end));
+	}
+}
