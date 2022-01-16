081d7732f378c9276aeb05e095cba6c87bc2117d
prgtest <marc.garcia0502@gmail.com>
2022-01-16 16:57:12 +0100

    prgtest autocommit #2 02_25: Ejercicio 02_25 acabado

--- 02_24_es_enter/UtilString.java
+++ 02_24_es_enter/UtilString.java
@@ -80,4 +80,18 @@ public class UtilString {
 		}
 		return true;
 	}
+
+	//Devuelve un String con la cadena de caracteres que ha  de printar
+	public static String cadenaContinua(String quants, String text) {
+		int numero = Integer.parseInt(quants);
+		String salida = "";
+		for (int i = 0; i < numero; i++) {
+		 	if (numero <= text.length()) {
+		 		salida = salida + text.charAt(i);
+		 	} else if (numero > text.length()) {
+		 		salida = salida + (text.charAt(i % text.length()));
+		 	}
+		}
+		return salida; 
+	}
 }
