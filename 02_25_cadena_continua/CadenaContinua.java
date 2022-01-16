cb0c379516423314914ca37541a09a66d4e172d6
Marc Garcia <marc.garcia0502@gmail.com>
2022-01-16 16:54:27 +0100

    Ejercicio 02_25 acabado

---
+++ 02_25_cadena_continua/CadenaContinua.java
@@ -0,0 +1,30 @@
+/*  Programa que hace una secuencia de caracteres con el numero de caracteres 
+	que le das
+*/
+public class CadenaContinua {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String entrada = Entrada.readLine();
+		System.out.println("Nombre?");
+		String quants = Entrada.readLine();
+		if (UtilString.esEnter(quants)) {
+			System.out.println(cadenaContinua(quants, entrada));
+		} else {
+			System.out.println("error");
+		}		
+	}
+
+	//Devuelve un String con la cadena de caracteres que ha  de printar
+	public static String cadenaContinua(String quants, String text) {
+		int numero = Integer.parseInt(quants);
+		String salida = "";
+		for (int i = 0; i < numero; i++) {
+		 	if (numero <= text.length()) {
+		 		salida = salida + text.charAt(i);
+		 	} else if (numero > text.length()) {
+		 		salida = salida + (text.charAt(i % text.length()));
+		 	}
+		}
+		return salida; 
+	}
+}
