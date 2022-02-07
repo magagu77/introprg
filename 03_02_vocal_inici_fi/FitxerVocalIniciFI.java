6ff8f94e93f3f7cfc674fb2c8cb4df74cd8ff1a2
Marc Garcia <marc.garcia0502@gmail.com>
2022-02-07 17:30:08 +0100

    Ejercicio 03_01 acabado

---
+++ 03_02_vocal_inici_fi/FitxerVocalIniciFI.java
@@ -0,0 +1,10 @@
+/* Programa del loro pero coge el texto de un archivo externo */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class FitxerVocalIniciFi {
+    public static void main(String[] args) throws IOException{
+        
+    }
+}
