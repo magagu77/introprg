01a7206ed73e349642aa6c30f530fbe1ba6b8d79
prgtest <marc.garcia0502@gmail.com>
2022-02-16 16:55:23 +0100

    prgtest autocommit #2 03_04: Ejercicio 03_04 acabado

--- 03_04_elpenjat/Penjat.java
+++ 03_04_elpenjat/Penjat.java
@@ -121,6 +121,7 @@ public class Penjat {
 	// Comprueba si tiene que parar la palabra y la partida
 	public static boolean comprovaSiPara (String para) {
 		if (para.equals("prou")) {
+			System.out.println("Vols finalitzar?");
 			if (UtilitatsConfirmacio.respostaABoolean(Entrada.readLine())) return true;
 			else return false;
 		} else return false;
