2818232d0b80217dd7b1a982e352ef214eea24cd
prgtest <marc.garcia0502@gmail.com>
2022-02-17 16:53:44 +0100

    prgtest autocommit #1 03_07: Ejercicio 03_07 acabado

--- 03_07_log/UsaLog.java
+++ 03_07_log/UsaLog.java
@@ -1,13 +1,17 @@
 /* Programa UsaLog para probar el Log.java */	
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 public class UsaLog {
-   public static void main(String[] args) throws IOException {
-       Log.printError("Això és un error greu");
-       Log.printWarning("Això és un avís");
-       for (int i=0; i < args.length; i++) {
+    public static void main(String[] args) throws IOException {
+        String fitxerLog ="log.txt";
+        BufferedWriter log = new BufferedWriter(new FileWriter(fitxerLog, true));
+        log.write(Log.printError("Això és un error greu"));
+        log.write(Log.printWarning("Això és un avís"));
+        for (int i=0; i < args.length; i++) {
            Log.printInfo(String.format("Argument %d: %s", i, args[i]));
-       }
-       Log.reset();     // comencem a comptar un altre cop
-       Log.printDebug("Aquí s'acaba el main()");
-   }
+        }
+        Log.reset();     // comencem a comptar un altre cop
+        log.write(Log.printDebug("Aquí s'acaba el main()"));
+    }
 }
