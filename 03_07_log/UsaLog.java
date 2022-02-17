f135e62893ae4cb5e4c9d01521791c39282a6851
prgtest <marc.garcia0502@gmail.com>
2022-02-17 16:58:29 +0100

    prgtest autocommit #2 03_07: Ejercicio 03_07 acabado

--- 03_07_log/UsaLog.java
+++ 03_07_log/UsaLog.java
@@ -7,11 +7,16 @@ public class UsaLog {
         String fitxerLog ="log.txt";
         BufferedWriter log = new BufferedWriter(new FileWriter(fitxerLog, true));
         log.write(Log.printError("Això és un error greu"));
+        log.newLine();
         log.write(Log.printWarning("Això és un avís"));
+        log.newLine();
         for (int i=0; i < args.length; i++) {
-           Log.printInfo(String.format("Argument %d: %s", i, args[i]));
+            log.write(Log.printInfo(String.format("Argument %d: %s", i, args[i])));
+            log.newLine();
         }
         Log.reset();     // comencem a comptar un altre cop
         log.write(Log.printDebug("Aquí s'acaba el main()"));
+        log.newLine();
+        log.close();
     }
 }
