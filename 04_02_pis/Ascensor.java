490d284e19b5bb63ef69e1caa5288d1024e5ae90
prgtest <marc.garcia0502@gmail.com>
2022-02-21 18:26:54 +0100

    prgtest autocommit #2 04_02: Ejercicio 04_02 acabado

--- 04_02_pis/Ascensor.java
+++ 04_02_pis/Ascensor.java
@@ -1,9 +1,9 @@
 /* Programa que muestra en que piso está un ascensor */
 public class Ascensor {
-    public int planta = -1;
+    public int pis = -1;
     public static void main(String[] args) {
         Ascensor ascensor;
         ascensor = new Ascensor();
-        System.out.println("L'ascensor està a la planta " + ascensor.planta);
+        System.out.println("L'ascensor està a la planta " + ascensor.pis);
     }  
 }
