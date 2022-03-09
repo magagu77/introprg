67afac87874f6cfe986b790a152f327b3d7e64d0
prgtest <marc.garcia0502@gmail.com>
2022-03-09 15:19:18 +0100

    prgtest autocommit #1 04_10: Ejercicio 04_10 acabado

--- 04_10_moviment/UsaAscensor.java
+++ 04_10_moviment/UsaAscensor.java
@@ -2,8 +2,6 @@
 public class UsaAscensor {
     public static void main(String[] args) {
         Ascensor ascensor = new Ascensor();
-        ascensor.pis = -1;
-        ascensor.moviment = "aturat";
         System.out.printf("Pis inicial: %s%n",ascensor.pis);
         System.out.printf("Moviment inicial: %s%n",ascensor.moviment);
         ascensor.moviment = "pujant";
