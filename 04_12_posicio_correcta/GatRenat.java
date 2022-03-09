65bc382ce2ef6271efdcf8d4972bdb01971bc673
Marc Garcia <marc.garcia0502@gmail.com>
2022-03-09 15:42:05 +0100

    Ejercicio 04_12 acabado

---
+++ 04_12_posicio_correcta/GatRenat.java
@@ -0,0 +1,18 @@
+// Clase GatRenat
+public class GatRenat {
+    private int vides = 7;
+    private String posicio = "estirat";
+    // Da el valor de las vidas 
+    public int getVides() {
+        return vides;
+    }
+    public void setVides(int valor) {
+        vides = valor;
+    }
+    public String getPosicio() {
+        return posicio;
+    }
+    public void setPosicio (String novaPosicio) {
+        posicio = novaPosicio; 
+    }
+}
