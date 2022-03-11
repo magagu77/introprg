7b51ddcab807835924e294536cd2e88c89b4a9ea
prgtest <marc.garcia0502@gmail.com>
2022-03-11 16:46:41 +0100

    prgtest autocommit #2 04_13: Ejercicio 04_13 acabado

--- 04_13_ascensor_protegit/UsaAscensor.java
+++ 04_13_ascensor_protegit/UsaAscensor.java
@@ -1,17 +1,20 @@
 // Programa que usa ascensor en e ejercicio 04_13
 public class UsaAscensor {
-    /* XXX */
     public static void main(String[] args) {
+        String moviments;
+        String nouPis;
         // Dona informació del pis en el que está i le moviment
         Ascensor ascensor = new Ascensor();
         System.out.println("Pis inicial: " + ascensor.getPis());
         System.out.println("Moviment inicial: " + ascensor.getMoviment());
         System.out.println("Introdueix nou pis:");
         // Nou pis
-        ascensor.setPis(Entrada.readLine());
+        nouPis = Entrada.readLine();
+        ascensor.setPis(nouPis);
         System.out.println("Introdueix nou moviment:");
         // Nou moviment
-        ascensor.setMoviment(Entrada.readLine());
+        moviments = Entrada.readLine();
+        ascensor.setMoviment(moviments);
         System.out.println("Pis final: " + ascensor.getPis());
         System.out.println("Moviment final: " + ascensor.getPis());
     }
