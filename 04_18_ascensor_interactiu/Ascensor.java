69e06170c3eb65b070b2b125d9f8e645defb9b7e
Marc Garcia <marc.garcia0502@gmail.com>
2022-03-21 18:13:21 +0100

    Ejercicio 04_18 acabado

---
+++ 04_18_ascensor_interactiu/Ascensor.java
@@ -0,0 +1,93 @@
+/* Ascensor que añade 4 modulos nuevos que pide el ejercicio 04_18, que devuelven un valor booleano segun si esta parado,
+ subiendo o bajando, o subir al ascensor al siguiente piso(en caso de llegear al piso 10 bajara al piso 9) */ 
+public class Ascensor {
+    private int pis = -1;
+    private String moviment = "aturat";
+
+    public int getPis() {
+        return pis;
+    }
+    public String getMoviment() {
+        return moviment;
+    }
+    public void setPis(int nouPis) {
+            if (nouPis <= 10 && nouPis >= -1) {
+                pis = nouPis;
+            }   
+    }
+    public void setMoviment(String newMoviment) {
+        if (newMoviment.equals("pujant") || newMoviment.equals("aturat") || newMoviment.equals("baixant")){
+            moviment = newMoviment;
+        }
+    }
+    public boolean estaAbaix() {
+        if (pis == -1) return true;
+        return false;
+    }
+    public boolean estaAmunt() {
+        if (pis == 10) return true;
+        return false;
+    }
+    public boolean estaAturat() {
+        if (moviment.equals("aturat")) return true;
+        return false;
+    }
+    public boolean estaPujant() {
+        if (moviment.equals("pujant")) return true;
+        return false;
+    }
+    public boolean estaBaixant() {
+        if (moviment.equals("baixant")) return true;
+        return false;
+    }
+    public String comEsta () {
+        String estat = "";
+        if (estaBaixant()) {
+            estat = estat + "baixant";
+        } else if (estaPujant()) {
+            estat = estat + "pujant";
+        } else if (estaAturat()) {
+            estat = estat + "aturat";
+        }
+        estat = estat + " al pis " + getPis(); 
+        return estat;
+    }
+    public boolean estaMovent() {
+        if (estaAturat()) return false;
+        else return true;
+    }
+    public boolean aturat() {
+        if (estaAturat()) return true;
+        else {
+            setMoviment("aturat");
+            return true;
+        }
+    }
+    public boolean arrencaAbaix(){
+        if (estaBaixant()) return true;
+        else {
+            setMoviment("baixant");
+            return true;
+        }
+    }
+    public boolean arrencaAmunt() {
+        if (estaAmunt()) return true;
+        else {
+            setMoviment("pujant");
+            return true;
+        }
+    }
+    public void seguentPis() {
+        if (estaPujant()) {
+            if(getPis() == 10) {
+                setMoviment("baixant");
+                setPis(getPis()-1);
+            } else setPis(getPis()+1);
+        } else if (estaBaixant()) {
+            if (getPis() == 0) {
+                setMoviment("pujant");
+                setPis(getPis()+1);
+            } else setPis(getPis()-1); 
+        }
+    }
+}
