6d7ddf52bb14e312b1f6b2648e8acd31e2e6e467
Marc Garcia <marc.garcia0502@gmail.com>
2022-03-24 16:49:00 +0100

    Ejercicio acabado

---
+++ 04_22_constructor_posicio/GatRenat.java
@@ -0,0 +1,34 @@
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    public GatRenat(int novesVides, String novaPosicio) {
+        vides = novesVides;
+        posicio = novaPosicio;
+    }
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posició: %s", vides, posicio);
+    }
+    public int getVides() { return vides; }
+
+    public String getPosicio() {return posicio;}
+
+    public void setPosicio (String novaPosicio) {
+        if (posicioCorrecta(novaPosicio)) {
+            posicio = novaPosicio; 
+        }
+        else {
+            posicio = "estirat";
+        }
+    }
+    //Comprova posicio correcta
+    public static boolean posicioCorrecta (String posicio) {
+        if(posicio.equals("dret") || posicio.equals("assegut") 
+        || posicio.equals("estirat")) return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "estirat"));
+    }
+}
