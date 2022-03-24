0b57c30260554c515d4dea14ec6390bd1f31d4ab
Marc Garcia <marc.garcia0502@gmail.com>
2022-03-24 17:05:15 +0100

    Ejercicio 04_23 acabado

---
+++ 04_23_aquesta_posicio/GatRenat.java
@@ -0,0 +1,36 @@
+/*  Ejercicio que comprueba que valor asigna en la posicio del gat renat y en caso de que no sea correcta le dice que esta estirado
+    esta versión usa el 'this' para llamr a una variable con el mismo nombre*/
+public class GatRenat {
+    private int vides;
+    private String posicio;
+    public GatRenat(int vides, String posicio) {
+        this.vides = vides;
+        setPosicio(posicio);
+    }
+    @Override
+    public String toString() {
+        return String.format("Vides: %d. Posició: %s", vides, posicio);
+    }
+    public int getVides() { return vides; }
+
+    public String getPosicio() {return posicio;}
+
+    public void setPosicio (String posicio) {
+        if (posicioCorrecta(posicio)) {
+            this.posicio = posicio; 
+        }
+        else {
+            this.posicio = "estirat";
+        }
+    }
+    //Comprova posicio correcta
+    public static boolean posicioCorrecta (String posicio) {
+        if(posicio.equals("dret") || posicio.equals("assegut") 
+        || posicio.equals("estirat")) return true; 
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GatRenat(7, "dret"));
+    }
+}
