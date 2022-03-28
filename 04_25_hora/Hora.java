b001e23980a1f13af4406821747218e3607ec365
prgtest <marc.garcia0502@gmail.com>
2022-03-28 11:48:36 +0200

    prgtest autocommit #13 04_25: Ejercicio 04_25 acabado

--- 04_25_hora/Hora.java
+++ 04_25_hora/Hora.java
@@ -105,7 +105,7 @@ public class Hora {
             setMinuts(getMinuts() + minuts);
         }
         if (getHores() + getHores() >= 24) {
-            setHores(hora-24);
+            setHores((getHores()-24) +hora);
         } else {
             setHores(getHores() + hora);
         }      
