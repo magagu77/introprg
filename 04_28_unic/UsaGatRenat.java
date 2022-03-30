71ccae43d9624ef24ac3bb75d91994680ad10ea1
prgtest <marc.garcia0502@gmail.com>
2022-03-30 16:14:59 +0200

    prgtest autocommit #3 04_28: Ejercicio 04_28 acabado

--- 04_28_unic/UsaGatRenat.java
+++ 04_28_unic/UsaGatRenat.java
@@ -14,11 +14,11 @@ public class UsaGatRenat {
     private static void canviaPosicio(String novaPosicio) {
         // Tradueix novaPosicio: 1 -> estirat, 2 -> assegut, 3 -> dret,
         // altrament es queda com estava
-        if (novaPosicio.equals("1")) {
+        if (novaPosicio.equals("1") || novaPosicio.equals("estirat")) {
             novaPosicio = "estirat";
-        } else if (novaPosicio.equals("2")) {
+        } else if (novaPosicio.equals("2") || novaPosicio.equals("assegut")) {
             novaPosicio = "assegut";
-        } else if (novaPosicio.equals("3")) {
+        } else if (novaPosicio.equals("3") || novaPosicio.equals("dret")) {
             novaPosicio = "dret";
         } else {
             return;
