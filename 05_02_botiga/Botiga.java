74dc4d01c29e29945d5e25d7766b6086676ba5db
prgtest <marc.garcia0502@gmail.com>
2022-04-16 23:48:38 +0200

    prgtest autocommit #27 05_02: Ejercicio 05_02 acabado

--- 05_02_botiga/Botiga.java
+++ 05_02_botiga/Botiga.java
@@ -60,7 +60,7 @@ public class Botiga {
         for(int i=0;i<vins.length;i++) {
             if(vins[i] == null) {
                 continue;
-            } else if (vins[i].getNom().toUpperCase().equals(nomVi.toUpperCase())) {
+            } else if (vins[i].getNom().toLowerCase().equals(nomVi.toLowerCase())) {
                 return vins[i];
             }
         }
