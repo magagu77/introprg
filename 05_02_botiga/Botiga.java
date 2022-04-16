a827fcac19dba66bead6b6d1753cc38579d3d2d6
Marc <marc.garcia0502@gmail.com>
2022-04-16 17:16:35 +0200

    Ejercicio a medias

---
+++ 05_02_botiga/Botiga.java
@@ -0,0 +1,15 @@
+/**
+ *  La clase botiga permet guardar un maxim de 10 vins en un array de Vi[], un metode afegeix
+ *  permetra afegir un nou vi a l' array, comprova que el vi sigui valid i que no esta repetit, si esta repetit, si
+ *  el vi introduit es corecte retorna el vi, en cas contrari retorna null
+ * 
+ *  El modul cerca() permet buscar un vi pel seu nom, y el retorna en cas que existeixi
+ * 
+ *  El modul elimina() un vi segons el nom que rebi, si no troba el vi retorna null, en cas que el vi existeixi y tingui stock
+ *  retornara null i no eliminara el vi, en cas qeu no hi hagi stock eliminara el vi
+ * 
+ *  cerca() i elimina() normalitzen el nom
+ */
+public class Botiga {
+
+}
