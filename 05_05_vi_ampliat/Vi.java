2bdcbd0bd6bd2b99daa9c19aebdcc413c2385f49
prgtest <marc.garcia0502@gmail.com>
2022-05-02 19:52:21 +0200

    prgtest autocommit #11 05_05: prgtest

--- 05_05_vi_ampliat/Vi.java
+++ 05_05_vi_ampliat/Vi.java
@@ -115,7 +115,7 @@ public class Vi {
 
     @Override
     public String toString() {
-        String vino = String.format("\n    Ref: %s\n    Nom: %s\n    Preu: %s\n    Estoc: %s\n    Lloc: %s\n    D.O.: %s\n    Tipus: %s\n",getRef(), getNom(),getPreu(),getEstoc(),getLloc(),getOrigen(),getTipus(),getCollita());
+        String vino = String.format("\n    Ref: %s\n    Nom: %s\n    Preu: %s\n    Estoc: %s\n    Lloc: %s\n    D.O.: %s\n    Tipus: %s\n   Collita: %s\n",getRef(), getNom(),getPreu(),getEstoc(),getLloc(),getOrigen(),getTipus(),getCollita(),getCollita());
         return vino;
     }
 }
