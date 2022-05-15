e84e4cb4a5103c75521ac0f217200d7366e24a49
Marc <marc.garcia0502@gmail.com>
2022-05-15 09:56:07 +0200

    Ejercicio acabado

---
+++ 52_03_coordenada_y/Punt.java
@@ -0,0 +1,16 @@
+/** Clase Punt per a probar que pasa tots els test de l'exercici 52_03 de tests */
+public class Punt {
+    private int y;
+    private int x;
+
+    public Punt(){
+        int y = 0;
+        int x = 0;
+    }
+    public int getY(){
+        return y;
+    }
+    public int getX(){
+        return x;
+    }
+}
---
+++ 52_03_coordenada_y/UsaPunt.java
@@ -0,0 +1,7 @@
+public class UsaPunt {
+    public static void main(String[] args){
+        Punt punt = new Punt();
+        System.out.printf("punt.getX() -> %d%n", punt.getX());
+        System.out.printf("punt.getY() -> %d%n", punt.getY());
+    }
+}
