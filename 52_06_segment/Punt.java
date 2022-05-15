2a50d1be9240b0833377146e62cecfc880c8fedd
Marc <marc.garcia0502@gmail.com>
2022-05-15 17:51:59 +0200

    Ejercicio acabado

---
+++ 52_06_segment/Punt.java
@@ -0,0 +1,41 @@
+/** Clase Punt per a probar que pasa tots els test de l'exercici 52_05 de tests */
+public class Punt {
+    private int y;
+    private int x;
+
+    // Constructor específic
+    public Punt(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    // Constructor per defecte
+    public Punt(){
+        int y = 0;
+        int x = 0;
+    }
+    // Getter Y
+    public int getY(){
+        return y;
+    }
+    // Getter X
+    public int getX(){
+        return x;
+    }
+    // Setter X
+    public void setX(int x) {
+        this.x=x;
+    }
+    // Setter Y
+    public void setY(int y) {
+        this.y=y;
+    }
+    // Suma dos puntos
+    public void suma(Punt p) { x+=p.x; y+=p.y; }
+
+    // ToString
+    @Override
+    public String toString() {
+        String punt = String.format("Punt(%s, %S)",getX(),getY());
+        return punt;
+    }
+}
