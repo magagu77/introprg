308c4f6ecc8aefe3f6c0f3c4332540947b9dab3e
Marc <marc.garcia0502@gmail.com>
2022-05-15 09:59:02 +0200

    Ejercicio acabado

---
+++ 52_03_coordenada_y/TestPunt.java
@@ -0,0 +1,24 @@
+import java.beans.Transient;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+public class TestPunt {
+    @Test
+    public void constructor() {
+        // comprova que Punt() pugui ser cridat
+        new Punt();
+    }
+
+    @Test
+    public void constructorDefecteXZero() {
+        // comprova que Punt() deixi a 0 la propietat x de Punt
+        Punt p = new Punt();
+        Assertions.assertEquals(0, p.getX());
+    }
+    @Test
+    public void constructorDefecteYZero() {
+        //Comprova que Punt() deixa a 0 la Y de punt
+        Punt p = new Punt();
+        Assertions.assertEquals(0,p.getY());
+    }
+}
