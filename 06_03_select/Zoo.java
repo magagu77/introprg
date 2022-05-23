84dcdffa58a56ff2a5532e34cda97623d3552825
Marc <marc.garcia0502@gmail.com>
2022-05-23 16:31:55 +0200

    Ejercicio acabdo

---
+++ 06_03_select/Zoo.java
@@ -0,0 +1,121 @@
+/** Clase zoo en l'exercici 06_02 que prova a conectarse a una base de dades SQLite per a guardar 
+ *  la taula categories, o eliminarla*/
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.LinkedList;
+import java.sql.ResultSet;
+public class Zoo {
+    private static final String NOM_BASE_DE_DADES = "animals.bd";
+    private static final String CADENA_DE_CONNEXIO = "jdbc:sqlite:" +
+                                                     NOM_BASE_DE_DADES;
+    private Connection conn = null;
+
+    public void connecta() throws SQLException {
+        if (conn != null) return;   // ja connectat
+        conn = DriverManager.getConnection(CADENA_DE_CONNEXIO);
+    }
+
+    public void desconnecta() throws SQLException {
+        if (conn == null) return; // ja desconnectat
+        conn.close();
+        conn = null;
+    }
+    public void creaTaulaCategories() throws SQLException {
+        eliminaTaulaCategories();
+        String sql = "CREATE TABLE  CATEGORIES (" +
+                     "       id        INTEGER PRIMARY KEY AUTOINCREMENT," +
+                     "       nom       VARCHAR(40))";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    public void eliminaTaulaCategories() throws SQLException {
+        String sql = "DROP TABLE IF EXISTS CATEGORIES";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(sql);
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    // Crea una lista amb les categories
+    public List<Categoria> recuperaCategories() throws SQLException {
+        String sql = "SELECT * FROM CATEGORIES ORDER BY nom";
+        Statement st = null;
+        try {
+            st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            List<Categoria> categories = new LinkedList<>();
+            while (rs.next()) {
+                int bdId = rs.getInt("id");
+                String nom = rs.getString("nom");
+                Categoria categoria = new Categoria(bdId, nom);
+                categories.add(categoria);
+            }
+            rs.close();
+            return categories;
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+    // Busca una categoria per nom
+    public Categoria obteCategoriaPerNom(String nom) throws SQLException {
+        String sql = "SELECT * FROM CATEGORIA ORDER BY ID WHERE LOWER(TRIM(nom))="+nom;
+        sql = "SELECT * FROM CATEGORIA ORDER BY ID WHERE LOWER(TRIM(nom))="+nom;
+        Statement st = null;
+        int contador = 0;
+        try {
+            st =conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if(rs.next()){
+                contador = rs.getInt(1);
+            } else {
+                return null;
+            }
+            if(contador == 0){return null;}
+            sql = "SELECT * FROM CATEGORIA ORDER BY ID WHERE LOWER(TRIM(nom))="+nom;
+            rs = st.executeQuery(sql);
+            List<Categoria> categories = new LinkedList<>();
+            while (rs.next()) {
+                int bdId = rs.getInt("id");
+                Categoria categoria = new Categoria(bdId, nom);
+                return categoria;
+            }
+            rs.close();
+            return null;
+        } finally {
+            if (st != null) {
+                st.close();
+            }
+        }
+    }
+        // Clase per a crear una categoria a la BDD
+        public void afegeixCategoria(Categoria categoria) throws SQLException {
+            String sql = String.format(
+                    "INSERT INTO CATEGORIES (nom) VALUES ('%s')",
+                    categoria.getNom());
+            Statement st = null;
+            try {
+                st = conn.createStatement();
+                st.executeUpdate(sql);
+            } finally {
+                if (st != null) {
+                    st.close();
+                }
+            }
+        }
+}
