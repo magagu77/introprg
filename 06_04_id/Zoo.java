10f66757c08c652a893e46b744715b7ec82f4363
prgtest <marc.garcia0502@gmail.com>
2022-05-23 18:38:21 +0200

    prgtest autocommit #11 06_04: Ejercicio acabado

--- 06_04_id/Zoo.java
+++ 06_04_id/Zoo.java
@@ -85,8 +85,10 @@ public class Zoo {
         // Clase per a crear una categoria a la BDD
         public void afegeixCategoria(Categoria categoria) throws SQLException {
             List<Categoria> categories = recuperaCategories();
-            int id = categories.size()+1;
-            categoria.setId(id);
+            if(categoria.idIndefinit()){
+                int id = categories.size()+1;
+                categoria.setId(id);
+            }
             String sql = String.format(
                     "INSERT INTO CATEGORIES (nom) VALUES ('%s')",
                     categoria.getNom());
