9a8aafca231cdb716306bc6b34ce775641c0582b
prgtest <marc.garcia0502@gmail.com>
2022-05-23 17:05:57 +0200

    prgtest autocommit #1 06_03: Ejercicio acabdo

--- 06_03_select/ZooUtils.java
+++ 06_03_select/ZooUtils.java
@@ -6,6 +6,10 @@ import java.util.LinkedList;
 public class ZooUtils {
     // Muestra las categorias por pantalla
     public static void mostraCategories(List<Categoria> categories) {
+        if(categories.isEmpty()){
+            System.out.println("Cap categoria");
+            return;
+        }
         for(Categoria categoria: categories){
             System.out.println(categoria);
         }
