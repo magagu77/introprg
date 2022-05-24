89bd1ab8d58eee3685003cc40cc2badb57c5359c
Marc <marc.garcia0502@gmail.com>
2022-05-24 19:44:17 +0200

    Ejercicio acabado

---
+++ 06_05_animals/ZooUtils.java
@@ -0,0 +1,16 @@
+/** Utilidades para Zoo y UsaZoo que estaran fuera de las otras clases */
+import java.util.List;
+import java.util.LinkedList;
+public class ZooUtils {
+    // Muestra las categorias por pantalla
+    public static void mostraCategories(List<Categoria> categories) {
+        if(categories.isEmpty()){
+            System.out.println("Cap categoria");
+            return;
+        }
+        System.out.println("Nombre de categories: "+categories.size());
+        for(Categoria categoria: categories){
+            System.out.println("\t"+categoria);
+        }
+    }
+}
