3111946f846e7501d19eb1bd70eb6412260394e8
prgtest <marc.garcia0502@gmail.com>
2022-05-24 20:12:53 +0200

    prgtest autocommit #3 06_06: Ejercicio acabado

--- 06_06_insert/Zoo.java
+++ 06_06_insert/Zoo.java
@@ -150,6 +150,12 @@ public class Zoo {
             rs.next();
             int id = rs.getInt(1);
             animal.setId(id);
+            sql = "SELECT COUNT(*) FROM CATEGORIES WHERE nom = "+animal.getCategoria().getNom();
+            rs = st.executeQuery(sql);
+            id = rs.getInt(1);
+            if(id == 0) {
+                afegeixCategoria(animal.getCategoria());
+            }
         } finally {
             if (st != null) {
                 st.close();
