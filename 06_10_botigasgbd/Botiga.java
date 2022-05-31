9045b0f634bdfa70dd2a7d684c2b8f17d2bace55
Marc <marc.garcia0502@gmail.com>
2022-05-31 16:01:26 +0200

    Ejercicio

---
+++ 06_10_botigasgbd/Botiga.java
@@ -0,0 +1,118 @@
+/**
+ *  La clase botiga permet guardar un maxim de 10 vins en un array de Vi[], un metode afegeix
+ *  permetra afegir un nou vi a l' array, comprova que el vi sigui valid i que no esta repetit, si esta repetit, si
+ *  el vi introduit es corecte retorna el vi, en cas contrari retorna null
+ * 
+ *  El modul cerca() permet buscar un vi pel seu nom, y el retorna en cas que existeixi
+ * 
+ *  El modul elimina() un vi segons el nom que rebi, si no troba el vi retorna null, en cas que el vi existeixi y tingui stock
+ *  retornara null i no eliminara el vi, en cas qeu no hi hagi stock eliminara el vi
+ * 
+ *  cerca() i elimina() normalitzen el nom
+ */
+public class Botiga {
+    private int DEFAULT_MAX_VINS = 10;
+    private Vi[] vins;
+    private int posicioArray = -1;
+
+    public Botiga() {
+        vins = new Vi[DEFAULT_MAX_VINS];
+    }
+    // Constructor sense vins per defecte
+    public Botiga(int maxVins) {
+        vins = new Vi[maxVins];
+    }
+
+    // Modul que afegeix vins
+    public Vi afegeix(Vi nouVi) {
+        if (!viEnLlista(vins, nouVi.getNom())) {
+            for(int i=0;i<vins.length;i++) {
+                if(vins[i]==null) {
+                    if(nouVi.esValid()) {
+                        vins[i] = nouVi;
+                        return vins[i];
+                    }
+                } else {
+                    continue;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    // Elimina vi
+    public Vi elimina(String nomVi) {
+        nomVi = normalitzaNom(nomVi);
+        for(int i=0;i<vins.length;i++) {
+            if (vins[i]==null){
+                continue;
+            } else if((vins[i].getNom().toLowerCase()).equals(nomVi.toLowerCase()) && vins[i].getEstoc() <= 0 ) {
+                Vi viEliminat = vins[i];
+                vins[i] = null;
+                return viEliminat;
+            }
+        }
+        return null;
+    }
+
+    public Vi cerca(String nomVi) {
+        nomVi = normalitzaNom(nomVi);
+        for(int i=0;i<vins.length;i++) {
+            if(vins[i] == null) {
+                continue;
+            } else if (vins[i].getNom().toLowerCase().equals(nomVi.toLowerCase())) {
+                return vins[i];
+            }
+        }
+        return null;
+    } 
+    public Vi modificaVi(String nom, int preu, int estoc) {
+        nom = normalitzaNom(nom);
+        for(int i=0;i<vins.length;i++) {
+            if(vins[i] == null) {
+                continue;
+            } else if (vins[i].getNom().toLowerCase().equals(nom.toLowerCase())) {
+                vins[i].setEstoc(estoc);
+                vins[i].setPreu(preu);
+                return vins[i];
+            }
+        }
+        return null;
+    }  
+
+    // Comprova si el vi esta en la llista
+    private boolean viEnLlista (Vi[] vins, String nomVi) {
+        for(int i=0;i<vins.length;i++) {
+            if(vins[i] == null) {
+                continue;
+            } else if (vins[i].getNom().equals(nomVi)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    // Normaliza nombres
+    public static String normalitzaNom(String nom) {
+        if (nom.isBlank()) {
+            return "NOM NO VÀLID!";
+        }
+        nom = nom.trim();
+        nom = String.format(nom.replaceAll(" +"," "));
+        return nom;
+    }
+    public void iniciaRecorregut() {
+        posicioArray = -1;
+    }
+    public Vi getSeguent() {
+        while (true) {
+            posicioArray = posicioArray + 1;
+            if(posicioArray >= vins.length) {return null;}
+            if (vins[posicioArray]==null) {continue;} 
+            else {
+            return vins[posicioArray];
+            }
+        }
+    }
+}
---
+++ 06_10_botigasgbd/Vi.java
@@ -0,0 +1,102 @@
+/**
+ * Clase vi que requereix un nom String, que no estigui en blanc i que al guardar el nom no es guardi amb cap espai 
+ * per davant o per darrere fet per un modul normalitzaNom()
+ * retornant NOM NO VALID! des d' un modul esValid() replaceAll()
+ * 
+ * preu, en centims i sense iva, no pot ser negatiu get preu&set preu, es determina si es valid pel petode es valid
+ * 
+ * Stock com a minim 0, mai menys, En cas que el constructor rebi un estoc negatiu, deixarà com a valor -1, el que 
+ * farà que la instància sigui considerada com no vàlida pel mètode esValid().
+ */
+public class Vi {
+
+    private String nom = "";
+    private int preu = -1;
+    private int estoc = -1; // Duele Moises, duele
+
+    // Constructor sense tenir en compte stock
+    public Vi(String nom, int preu) {
+        setPreu(preu);
+        this.nom = normalitzaNom(nom);
+        estoc = 0;
+
+    }
+    // Constructor con stock
+    public Vi(String nom, int preu, int estoc) {
+        this.nom = normalitzaNom(nom);
+        setPreu(preu);
+        setEstoc(estoc);
+    }
+
+    // Getters
+    public String getNom() {return nom;}
+
+    public int getPreu() {return preu;}
+
+    public int getEstoc() {return estoc;}
+
+
+    // Setters
+    public void setNom(String nom) {
+        if(!nom.isBlank()){
+            this.nom = nom;
+        }
+    }
+    public void setPreu(int preu) {
+        if (preu >= 0) {
+            this.preu = preu;
+        }
+    }
+    public void setEstoc(int stock) {
+        if(stock >= 0) {
+            this.estoc = stock;
+        }
+    }
+    // Normaliza String del nombre
+    public static String normalitzaNom(String nom) {
+        if (nom.isBlank()) {
+            return "NOM NO VÀLID!";
+        }
+        nom = nom.trim();
+        nom = String.format(nom.replaceAll(" +"," "));
+        return nom;
+    }
+    // Determina si el preu d'un vi es valid o no
+    public boolean esValid() {
+        if (preu < 0) {
+            return false;
+        } else if (nom.isBlank()){
+            return false;
+        } else if (nom.equals("NOM NO VÀLID!")) {
+            return false;
+        } else if (estoc < 0){
+            return false;
+        } else {
+            return true;
+        }
+    }
+    // Funcion que recibe un array de Strings y los convierte en un vino
+
+    /** PENDIENTE ARREGLAR */
+    public static Vi deArrayString(String[] vins) {
+        if(vins.length != 3){return null;}
+        if(vins[0].isBlank()|| vins[0]==null){return null;}
+        if(!UtilString.esEnter(vins[1])|| vins[1]==null){return null;}
+        if(!UtilString.esEnter(vins[2])||vins[2]==null){return null;}
+        Vi nouVi = new Vi(normalitzaNom(vins[0]),Integer.parseInt(vins[1]),Integer.parseInt(vins[2]));
+        if (nouVi.esValid()) {return nouVi;}
+        else {return null;}
+    }
+    // Convierte las instancias de un vino en un array de Strings 
+    public String[] aArrayString() {
+        String[] viString ={ getNom(),Integer.toString(getPreu()),Integer.toString(getEstoc())};
+        return viString;
+        
+    }
+
+    @Override
+    public String toString() {
+        String vino = String.format("\n    Vi: %s\n    Preu: %s\n    Estoc: %s\n", getNom(),getPreu(),getEstoc());
+        return vino;
+    }
+}
