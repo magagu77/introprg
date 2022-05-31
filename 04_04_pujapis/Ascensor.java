7c375ebe3a8df7c8b895dffb5f529f4e782b5f67
Marc <marc.garcia0502@gmail.com>
2022-05-31 16:01:26 +0200

    Ejercicio

--- 04_04_pujapis/Ascensor.java
+++ 04_04_pujapis/Ascensor.java
@@ -1,12 +1,10 @@
-// Programa del ascensor pero puja un pis
 public class Ascensor {
-    public static int pis = -1;
+    int pis = -1;
     public static void pujaPis(Ascensor ascensor) {
-        ascensor.pis++;
+        ascensor.pis++; 
     }
     public static void main(String[] args){
-        Ascensor ascensor;
-        ascensor = new Ascensor();
+        Ascensor ascensor = new Ascensor();
         System.out.println("L'ascensor inicialment està a la planta " + ascensor.pis);
         pujaPis(ascensor);
         System.out.println("L'ascensor finalment està a la planta " + ascensor.pis);
