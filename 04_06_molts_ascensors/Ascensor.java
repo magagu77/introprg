07d08f13dc11e2eb7e9e13851be97082e8c9aeb0
Marc <marc.garcia0502@gmail.com>
2022-05-31 16:15:48 +0200

    prgtest

--- 04_06_molts_ascensors/Ascensor.java
+++ 04_06_molts_ascensors/Ascensor.java
@@ -2,23 +2,22 @@
 public class Ascensor {
     public int pis;
     // AScensor puja un pis
+    public Ascensor(int pis){
+        this.pis = pis; 
+    }
     public static Ascensor[] creaAscensors(int quants) {
         Ascensor[] ascensors = new Ascensor[quants];
         for (int i=0;i<quants;i++) {
-            ascensors[i] = new Ascensor();
-            ascensors[i].pis = i;
-        }       
+            ascensors[i] = new Ascensor(i);
+        }
         return ascensors;
     }
-    public static void main(String[] args) {
-        if(!UtilString.esEnter(args[0]) || args[0].equals("0")) {
-            System.out.println("Cap ascensor");
-            return;
-        }
-        int quants = Integer.parseInt(args[0]);
+
+    public static void main(String[] args){
+        int quants = Integer.parseInt(args[0]); 
         Ascensor[] ascensors = creaAscensors(quants);
         for (int i = 0; i < ascensors.length; i++) {
-            System.out.printf("Ascensor %s al pis %s%n", i, ascensors[i].pis);
+            System.out.printf("Ascensor %d al pis %d%n", i, ascensors[i].pis);
         }
     }
 }
